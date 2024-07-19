b2743453fc74d0b06e7e38083b4c243bff95d132
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-07-19 20:20:17 +0400

    06-react-suspense: suspense image solution

--- 06-react-suspense/src/exercise/05.extra-1.js
+++ 06-react-suspense/src/exercise/05.extra-1.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import {
   fetchPokemon,
+  getImageUrlForPokemon,
   PokemonInfoFallback,
   PokemonForm,
   PokemonDataView,
@@ -22,25 +23,12 @@ function preloadImage(src) {
   })
 }
 
-const imgSrcResourceCache = {}
-
-function Img({src, alt, props}) {
-  let imgSrcResource = imgSrcResourceCache[src]
-
-  if (!imgSrcResource) {
-    imgSrcResource = createResource(preloadImage(src))
-    imgSrcResourceCache[src] = imgSrcResource
-  }
-
-  return <img src={imgSrcResource.read()} alt={alt} {...props} />
-}
-
 function PokemonInfo({pokemonResource}) {
-  const pokemon = pokemonResource.read()
+  const pokemon = pokemonResource.data.read()
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
-        <Img src={pokemon.image} alt={pokemon.name} />
+        <img src={pokemonResource.image.read()} alt={pokemon.name} />
       </div>
       <PokemonDataView pokemon={pokemon} />
     </div>
@@ -66,7 +54,10 @@ function getPokemonResource(name) {
 }
 
 function createPokemonResource(pokemonName) {
-  return createResource(fetchPokemon(pokemonName))
+  const data = createResource(fetchPokemon(pokemonName))
+  const image = createResource(preloadImage(getImageUrlForPokemon(pokemonName)))
+
+  return {data, image}
 }
 
 function App() {
