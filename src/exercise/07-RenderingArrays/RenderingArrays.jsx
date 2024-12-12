01a9c00ac2fedef9068b347a66e9093fb3f2b4ef
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-12-12 16:50:49 +0400

    update RenderingArrays

--- src/exercise/07-RenderingArrays/RenderingArrays.jsx
+++ src/exercise/07-RenderingArrays/RenderingArrays.jsx
@@ -1,4 +1,4 @@
-// import { ItemsList } from './ItemsList'
-import { ItemsList } from './ItemsList.extra-01'
+import { ItemsList } from './ItemsList'
+// import { ItemsList } from './ItemsList.extra-01'
 
 export const RenderingArrays = () => <ItemsList />
